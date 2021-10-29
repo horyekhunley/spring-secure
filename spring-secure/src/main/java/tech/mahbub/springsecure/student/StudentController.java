20ea7c81df4bbee8146046287ce9dead4807e238
horyekhunley <mahbubbello@gmail.com>
2021-10-29 06:38:21 +0100

    Add files via upload

---
+++ spring-secure/src/main/java/tech/mahbub/springsecure/student/StudentController.java
@@ -0,0 +1,29 @@
+package tech.mahbub.springsecure.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+      new Student(1, "James Bond"),
+            new Student(2, "Tunde Kelani"),
+      new Student(3, "Mahbub Bello")
+
+    );
+
+    @GetMapping(path="{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist"));
+    }
+}
